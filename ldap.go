c2a3180ebab4892e5b2a6c868f0cddba42957b30
Brian Samek <brian.samek@mongodb.com>
2018-09-10 13:42:06 -0400

    EVG-5295 Write function to authenticate user against LDAP

---
+++ ldap.go
@@ -0,0 +1,61 @@
+package gimlet
+
+import (
+	"crypto/tls"
+	"fmt"
+
+	"github.com/pkg/errors"
+	ldap "gopkg.in/ldap.v2"
+)
+
+// LDAPAuthenticator provides authentication and authorization of users against an LDAP service.
+type LDAPAuthenticator struct {
+	url  string
+	port string
+	path string
+	conn *ldap.Conn
+}
+
+// NewLDAPAuthenticator constructs an LDAPAuthenticator. It requires a url and port to the LDAP
+// server. It also requires a path to user resources that can be passed to an LDAP query.
+func NewLDAPAuthenticator(url, port, path string) (*LDAPAuthenticator, error) {
+	if url == "" || port == "" || path == "" {
+		return nil, errors.Errorf("url ('%s'), port ('%s'), and path ('%s') must be provided", url, port, path)
+	}
+	return &LDAPAuthenticator{
+		url:  url,
+		port: port,
+		path: path,
+	}, nil
+}
+
+// Authenticate returns nil if the user and password are valid, an error otherwise.
+func (l *LDAPAuthenticator) Authenticate(user, password string) error {
+	if err := l.connect(); err != nil {
+		return errors.Wrap(err, "could not connect to LDAP server")
+	}
+	if err := l.login(user, password); err != nil {
+		return errors.Wrap(err, "failed to validate user")
+	}
+	return nil
+}
+
+// Authorize returns nil if the user is a member of the group, an error otherwise.
+func (l *LDAPAuthenticator) Authorize(user, group string) error {
+	return errors.New("not implemented")
+}
+
+func (l *LDAPAuthenticator) connect() error {
+	tlsConfig := &tls.Config{ServerName: l.url}
+	conn, err := ldap.DialTLS("tcp", fmt.Sprintf("%s:%s", l.url, l.port), tlsConfig)
+	if err != nil {
+		return errors.Wrapf(err, "problem connecting to ldap server %s:%s", l.url, l.port)
+	}
+	l.conn = conn
+	return nil
+}
+
+func (l *LDAPAuthenticator) login(user, password string) error {
+	fullPath := fmt.Sprintf("uid=%s,%s", user, l.path)
+	return errors.Wrapf(l.conn.Bind(fullPath, password), "could not validate user '%s'", user)
+}
---
+++ ldap_test.go
@@ -0,0 +1,44 @@
+package gimlet
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLDAPConstructorRequiresNonEmptyArgs(t *testing.T) {
+	assert := assert.New(t)
+
+	l, err := NewLDAPAuthenticator("foo", "bar", "baz")
+	assert.NotNil(l)
+	assert.NoError(err)
+
+	l, err = NewLDAPAuthenticator("", "bar", "baz")
+	assert.Nil(l)
+	assert.Error(err)
+
+	l, err = NewLDAPAuthenticator("foo", "", "baz")
+	assert.Nil(l)
+	assert.Error(err)
+
+	l, err = NewLDAPAuthenticator("foo", "bar", "")
+	assert.Nil(l)
+	assert.Error(err)
+}
+
+// This test requires an LDAP server. Uncomment to test.
+//
+// func TestLDAPIntegration(t *testing.T) {
+// 	const (
+// 		url      = ""
+// 		port     = ""
+// 		path     = ""
+// 		username = ""
+// 		password = ""
+// 	)
+// 	assert := assert.New(t)
+// 	l, err := NewLDAPAuthenticator(url, port, path)
+// 	assert.NotNil(l)
+// 	assert.NoError(err)
+// 	assert.NoError(l.Authenticate())
+// }
