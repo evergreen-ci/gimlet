47d1215e2f268e2f230dc3de7c2973d232cab810
julianedwards <julian.edwards@mongodb.com>
2019-03-26 14:44:28 -0400

    add password to constructor for basic user

--- auth_user_basic.go
+++ auth_user_basic.go
@@ -2,11 +2,12 @@ package gimlet
 
 // NewBasicUser constructs a simple user. The underlying type has
 // serialization tags.
-func NewBasicUser(id, name, email, key string, roles []string) User {
+func NewBasicUser(id, name, email, password, key string, roles []string) User {
 	return &basicUser{
 		ID:           id,
 		Name:         name,
 		EmailAddress: email,
+		Password:     password,
 		Key:          key,
 		AccessRoles:  roles,
 	}
@@ -18,11 +19,11 @@ func MakeBasicUser() User { return &basicUser{} }
 
 type basicUser struct {
 	ID           string   `bson:"_id" json:"id" yaml:"id"`
-	EmailAddress string   `bson:"email" json:"email" yaml:"email"`
 	Name         string   `bson:"name" json:"name" yaml:"name"`
+	EmailAddress string   `bson:"email" json:"email" yaml:"email"`
+	Password     string   `bson:"password" json:"password" yaml:"password"`
 	Key          string   `bson:"key" json:"key" yaml:"key"`
 	AccessRoles  []string `bson:"roles" json:"roles" yaml:"roles"`
-	Password     string   `bson:"password" json:"password" yaml:"password"`
 	Invalid      bool     `bson:"invalid" json:"invalid" yaml:"invalid"`
 }
 
