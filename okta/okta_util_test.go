1d67c60741154543ef415953e6e65e8e8bbb19fd
Kim Tao <kim.tao@mongodb.com>
2020-01-09 15:51:56 -0500

    Fix random test

--- okta/okta_util_test.go
+++ okta/okta_util_test.go
@@ -2,7 +2,6 @@ package okta
 
 import (
 	"encoding/base64"
-	"encoding/hex"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,28 +9,12 @@ import (
 )
 
 func TestRandom(t *testing.T) {
-	for funcName, testParams := range map[string]struct {
-		randFunc func() (string, error)
-		length   int
-	}{
-		"RandomString": {
-			randFunc: randomString,
-			length:   hex.EncodedLen(16),
-		},
-		"GenerateNonce": {
-			randFunc: generateNonce,
-			length:   base64.URLEncoding.EncodedLen(32),
-		},
-	} {
-		t.Run(funcName, func(t *testing.T) {
-			prev := []string{}
-			for i := 0; i < 1000; i++ {
-				s, err := testParams.randFunc()
-				require.NoError(t, err)
-				assert.Len(t, s, testParams.length)
-				assert.NotContains(t, prev, s)
-				prev = append(prev, s)
-			}
-		})
+	prev := []string{}
+	for i := 0; i < 1000; i++ {
+		s, err := randomString()
+		require.NoError(t, err)
+		assert.Len(t, s, base64.URLEncoding.EncodedLen(32))
+		assert.NotContains(t, prev, s)
+		prev = append(prev, s)
 	}
 }
