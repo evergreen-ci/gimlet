0c47ad13e171441e8cf4ba35fe0e5f40610bf540
Kim Tao <kim.tao@mongodb.com>
2020-02-26 11:41:18 -0500

    Separate writer managers from reader managers to avoid possibly unwanted side effects

--- user_service_multi.go
+++ user_service_multi.go
@@ -9,21 +9,26 @@ import (
 )
 
 type multiUserManager struct {
-	managers []UserManager
+	primary     UserManager
+	secondaries []UserManager
 }
 
 // NewMultiUserManager multiplexes several UserManagers into a single
-// UserManager. Each method invoked on the returned UserManager is run against
-// all managers until one returns a valid result. Managers are prioritized in
-// the same order in which they are passed into this function.
-func NewMultiUserManager(managers ...UserManager) (UserManager, error) {
-	return nil, nil
+// UserManager. For write operations, UserManager methods are only invoked on
+// the primary UserManager. For read operations, the UserManager runs the method
+// against all managers until one returns a valid result. Managers are
+// prioritized in the same order in which they are passed into this function.
+func NewMultiUserManager(primary UserManager, secondaries ...UserManager) UserManager {
+	return &multiUserManager{
+		primary:     primary,
+		secondaries: secondaries,
+	}
 }
 
 func (um *multiUserManager) GetUserByToken(ctx context.Context, token string) (User, error) {
 	var u User
 	var err error
-	if err = um.tryManagers(func(m UserManager) (bool, error) {
+	if err = um.tryAllManagers(func(m UserManager) (bool, error) {
 		u, err = m.GetUserByToken(ctx, token)
 		return err == nil, err
 	}); err != nil {
@@ -33,20 +38,12 @@ func (um *multiUserManager) GetUserByToken(ctx context.Context, token string) (U
 }
 
 func (um *multiUserManager) CreateUserToken(username, password string) (string, error) {
-	var token string
-	var err error
-	if err = um.tryManagers(func(m UserManager) (bool, error) {
-		token, err = m.CreateUserToken(username, password)
-		return err == nil, err
-	}); err != nil {
-		return "", errors.Wrap(err, "failed to create user token")
-	}
-	return token, nil
+	return um.primary.CreateUserToken(username, password)
 }
 
 func (um *multiUserManager) GetLoginHandler(rootURL string) http.HandlerFunc {
 	var handler http.HandlerFunc
-	_ = um.tryManagers(func(m UserManager) (bool, error) {
+	_ = um.tryAllManagers(func(m UserManager) (bool, error) {
 		handler = m.GetLoginHandler("")
 		return handler == nil, nil
 	})
@@ -55,7 +52,7 @@ func (um *multiUserManager) GetLoginHandler(rootURL string) http.HandlerFunc {
 
 func (um *multiUserManager) GetLoginCallbackHandler() http.HandlerFunc {
 	var handler http.HandlerFunc
-	_ = um.tryManagers(func(m UserManager) (bool, error) {
+	_ = um.tryAllManagers(func(m UserManager) (bool, error) {
 		handler = m.GetLoginCallbackHandler()
 		return handler == nil, nil
 	})
@@ -64,7 +61,7 @@ func (um *multiUserManager) GetLoginCallbackHandler() http.HandlerFunc {
 
 func (um *multiUserManager) IsRedirect() bool {
 	var isRedirect bool
-	_ = um.tryManagers(func(m UserManager) (bool, error) {
+	_ = um.tryAllManagers(func(m UserManager) (bool, error) {
 		isRedirect = m.IsRedirect()
 		return isRedirect, nil
 	})
@@ -73,7 +70,7 @@ func (um *multiUserManager) IsRedirect() bool {
 
 func (um *multiUserManager) ReauthorizeUser(u User) error {
 	var err error
-	if err = um.tryManagers(func(m UserManager) (bool, error) {
+	if err = um.tryAllManagers(func(m UserManager) (bool, error) {
 		err = m.ReauthorizeUser(u)
 		return err == nil, err
 	}); err != nil {
@@ -85,7 +82,7 @@ func (um *multiUserManager) ReauthorizeUser(u User) error {
 func (um *multiUserManager) GetUserByID(id string) (User, error) {
 	var u User
 	var err error
-	if err = um.tryManagers(func(m UserManager) (bool, error) {
+	if err = um.tryAllManagers(func(m UserManager) (bool, error) {
 		u, err = m.GetUserByID(id)
 		return err == nil, err
 	}); err != nil {
@@ -95,32 +92,17 @@ func (um *multiUserManager) GetUserByID(id string) (User, error) {
 }
 
 func (um *multiUserManager) GetOrCreateUser(u User) (User, error) {
-	var newUser User
-	var err error
-	if err = um.tryManagers(func(m UserManager) (bool, error) {
-		newUser, err = m.GetOrCreateUser(u)
-		return err == nil, err
-	}); err != nil {
-		return nil, errors.Wrap(err, "could not get or create user")
-	}
-	return newUser, nil
+	return um.primary.GetOrCreateUser(u)
 }
 
 func (um *multiUserManager) ClearUser(u User, all bool) error {
-	return um.tryManagers(func(m UserManager) (bool, error) {
-		err := m.ClearUser(u, all)
-		if err == nil {
-			return false, err
-		}
-		return err == nil, err
-	})
-
+	return um.primary.ClearUser(u, all)
 }
 
 func (um *multiUserManager) GetGroupsForUser(username string) ([]string, error) {
 	var groups []string
 	var err error
-	if err = um.tryManagers(func(m UserManager) (bool, error) {
+	if err = um.tryAllManagers(func(m UserManager) (bool, error) {
 		groups, err = m.GetGroupsForUser(username)
 		return err == nil, err
 	}); err != nil {
@@ -129,12 +111,12 @@ func (um *multiUserManager) GetGroupsForUser(username string) ([]string, error) 
 	return groups, nil
 }
 
-// tryManagers runs a function on each managers until either managerFunc tells
+// tryAllManagers runs a function on each managers until either managerFunc tells
 // it to stop (which is considered success) or it has tried and failed the
 // operation on all managers.
-func (um *multiUserManager) tryManagers(managerFunc func(UserManager) (stop bool, err error)) error {
+func (um *multiUserManager) tryAllManagers(managerFunc func(UserManager) (stop bool, err error)) error {
 	catcher := grip.NewBasicCatcher()
-	for _, m := range um.managers {
+	for _, m := range append([]UserManager{um.primary}, um.secondaries...) {
 		stop, err := managerFunc(m)
 		if err == nil {
 			return nil
