4652dc19890fcae265c4b8bc362472978eb01fa6
Kim Tao <kim.tao@mongodb.com>
2023-11-08 15:20:58 -0500

    Fix tests, 'reqest', and 'middlewear'

--- middleware_grip_test.go
+++ middleware_grip_test.go
@@ -2,7 +2,6 @@ package gimlet
 
 import (
 	"context"
-	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
@@ -16,18 +15,19 @@ import (
 	"github.com/mongodb/grip/message"
 	"github.com/mongodb/grip/send"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestReqestLogger(t *testing.T) {
+func TestRequestLogger(t *testing.T) {
 	assert := assert.New(t)
 
 	sender, err := send.NewInternalLogger("test", grip.GetSender().Level())
 	assert.NoError(err)
-	middlewear := NewAppLogger().(*appLogging)
-	middlewear.Journaler = logging.MakeGrip(sender)
+	middleware := NewAppLogger().(*appLogging)
+	middleware.Journaler = logging.MakeGrip(sender)
 
 	next := func(w http.ResponseWriter, r *http.Request) {
-		middlewear.Journaler.Info("hello")
+		middleware.Journaler.Info("hello")
 	}
 	assert.False(sender.HasMessage())
 	req := &http.Request{
@@ -36,21 +36,21 @@ func TestReqestLogger(t *testing.T) {
 	rw := negroni.NewResponseWriter(nil)
 
 	startAt := getNumber()
-	middlewear.ServeHTTP(rw, req, next)
+	middleware.ServeHTTP(rw, req, next)
 	assert.Equal(startAt+2, getNumber())
 	assert.True(sender.HasMessage())
-	assert.Equal(sender.Len(), 3)
+	assert.Equal(sender.Len(), 2)
 }
 
-func TestReqestPanicLogger(t *testing.T) {
+func TestRequestPanicLogger(t *testing.T) {
 	assert := assert.New(t)
 
 	sender, err := send.NewInternalLogger("test", grip.GetSender().Level())
 	assert.NoError(err)
-	middlewear := NewRecoveryLogger(logging.MakeGrip(sender)).(*appRecoveryLogger)
+	middleware := NewRecoveryLogger(logging.MakeGrip(sender)).(*appRecoveryLogger)
 
 	next := func(w http.ResponseWriter, r *http.Request) {
-		middlewear.Journaler.Info("hello")
+		middleware.Journaler.Info("hello")
 	}
 	assert.False(sender.HasMessage())
 	req := &http.Request{
@@ -59,18 +59,19 @@ func TestReqestPanicLogger(t *testing.T) {
 	rw := negroni.NewResponseWriter(nil)
 
 	startAt := getNumber()
-	middlewear.ServeHTTP(rw, req, next)
+	middleware.ServeHTTP(rw, req, next)
 	assert.Equal(startAt+2, getNumber())
 	assert.True(sender.HasMessage())
-	assert.Equal(sender.Len(), 3)
+	assert.Equal(sender.Len(), 2)
 }
 
-func TestReqestPanicLoggerWithPanic(t *testing.T) {
+func TestRequestPanicLoggerWithPanic(t *testing.T) {
 	assert := assert.New(t)
+	require := require.New(t)
 
 	sender, err := send.NewInternalLogger("test", grip.GetSender().Level())
 	assert.NoError(err)
-	middlewear := NewRecoveryLogger(logging.MakeGrip(sender))
+	middleware := NewRecoveryLogger(logging.MakeGrip(sender))
 
 	next := func(w http.ResponseWriter, r *http.Request) {
 		panic("oops")
@@ -84,25 +85,26 @@ func TestReqestPanicLoggerWithPanic(t *testing.T) {
 	rw := negroni.NewResponseWriter(testrw)
 
 	startAt := getNumber()
-	middlewear.ServeHTTP(rw, req, next)
+	middleware.ServeHTTP(rw, req, next)
 
 	assert.Equal(startAt+2, getNumber())
 	assert.True(sender.HasMessage())
-	assert.Equal(sender.Len(), 2)
+	assert.Equal(sender.Len(), 1)
 
 	m, ok := sender.GetMessageSafe()
-	assert.True(ok)
-	assert.NotNil(m)
+	require.True(ok)
+	require.NotNil(m)
 	_, ok = m.Message.Raw().(message.Fields)
 	assert.True(ok)
 }
 
-func TestReqestPanicLoggerWithErrAbortHandler(t *testing.T) {
+func TestRequestPanicLoggerWithErrAbortHandler(t *testing.T) {
 	assert := assert.New(t)
+	require := require.New(t)
 
 	sender, err := send.NewInternalLogger("test", grip.GetSender().Level())
 	assert.NoError(err)
-	middlewear := NewRecoveryLogger(logging.MakeGrip(sender))
+	middleware := NewRecoveryLogger(logging.MakeGrip(sender))
 
 	next := func(w http.ResponseWriter, r *http.Request) {
 		panic(http.ErrAbortHandler)
@@ -115,20 +117,18 @@ func TestReqestPanicLoggerWithErrAbortHandler(t *testing.T) {
 	rw := negroni.NewResponseWriter(testrw)
 
 	startAt := getNumber()
-	middlewear.ServeHTTP(rw, req, next)
+	middleware.ServeHTTP(rw, req, next)
 
 	assert.Equal(startAt+2, getNumber())
 	assert.True(sender.HasMessage())
 
-	// get the second message
-	assert.Equal(sender.Len(), 2)
-	_, _ = sender.GetMessageSafe()
+	assert.Equal(sender.Len(), 1)
 	m, ok := sender.GetMessageSafe()
-	assert.True(ok)
-	assert.NotNil(m)
+	require.True(ok)
+	require.NotNil(m)
 	assert.Equal(level.Debug, m.Priority)
 	fields, ok := m.Message.Raw().(message.Fields)
-	assert.True(ok)
+	require.True(ok)
 	assert.Equal("hit suppressed abort panic", fields["message"])
 }
 
@@ -182,10 +182,11 @@ func TestLoggingAnnotations(t *testing.T) {
 
 func TestLoggingAnnotation(t *testing.T) {
 	assert := assert.New(t)
+	require := require.New(t)
 
 	sender, err := send.NewInternalLogger("test", grip.GetSender().Level())
 	assert.NoError(err)
-	middlewear := NewRecoveryLogger(logging.MakeGrip(sender))
+	middleware := NewRecoveryLogger(logging.MakeGrip(sender))
 
 	var called bool
 	next := func(w http.ResponseWriter, r *http.Request) {
@@ -203,26 +204,19 @@ func TestLoggingAnnotation(t *testing.T) {
 	rw := negroni.NewResponseWriter(testrw)
 
 	startAt := getNumber()
-	middlewear.ServeHTTP(rw, req, next)
+	middleware.ServeHTTP(rw, req, next)
 	assert.True(called)
 
 	assert.Equal(startAt+2, getNumber())
 	assert.True(sender.HasMessage())
-	assert.Equal(sender.Len(), 2)
+	assert.Equal(sender.Len(), 1)
 
-	// first message is the started one
 	m, ok := sender.GetMessageSafe()
-	assert.True(ok)
-	assert.NotNil(m)
-
-	// get the second message for the completed
-	m, ok = sender.GetMessageSafe()
-	assert.True(ok)
-	assert.NotNil(m)
+	require.True(ok)
+	require.NotNil(m)
 
 	fields, ok := m.Message.Raw().(message.Fields)
 	assert.True(ok)
-	fmt.Println(fields)
 	val, ok := fields["key"]
 	assert.True(ok)
 	assert.Equal("value", val)
